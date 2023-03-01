5cb17ec08c803341a6265eb133e56bc03de56d0b
aBusOfDucks <filip.nogaj272@gmail.com>
2023-03-01 22:06:35 +0100

    Added: - pressing 1,2,...,9,0 changes theme (colors of background and cells). - pressing 'C' clears the board. Changes: - deleted concurrent_manager class - now game_of_life class manages concurrent access to restarting the game, ending the game etc.

--- game_of_life.cpp
+++ game_of_life.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <condition_variable>
 #include <random>
+#include <iostream>
 
 // Size (in cells) of a board.
 #define WIDTH 80
@@ -21,15 +22,30 @@
 // Only works for n >= 1.
 #define SPEED 10
 
-// Default colors of cells and background.
-#define CELL_COLOR al_map_rgb(255, 255, 255)
-#define BACKGROUND_COLOR al_map_rgb(0, 0, 0)
-
 // Class implementing Conway's Game of Life.
 class game_of_life{
 private:
-	bool cells[WIDTH + 7][HEIGHT + 7];
-	bool new_cells[WIDTH + 7][HEIGHT + 7];
+	bool cells[WIDTH + 2][HEIGHT + 2];
+	bool new_cells[WIDTH + 2][HEIGHT + 2];
+	std::random_device dev;
+	std::mt19937 rng;
+	std::uniform_int_distribution<std::mt19937::result_type> cell_generator;
+	std::uniform_int_distribution<std::mt19937::result_type> color_generator;
+	
+	int cell_rainbow[3];
+	int cell_rainbow_current;
+	int cell_rainbow_mode;
+	
+	int background_rainbow[3];
+	int background_rainbow_current;
+	int background_rainbow_mode;
+	
+	std::mutex mut;
+	bool end_of_game = false;
+	bool pause_state = false;
+	bool restart_state = false;
+	bool clear_state = false;
+	int theme = 1;
 	
 	int neighbours(int x, int y)
 	{
@@ -41,35 +57,155 @@ private:
 		return ans;
 	}
 	
+	ALLEGRO_COLOR get_background_color(int theme)
+	{
+		background_rainbow[background_rainbow_current] += background_rainbow_mode;
+		if(background_rainbow[background_rainbow_current] % 250 == 0)
+		{
+			background_rainbow_current++;
+			background_rainbow_current %= 3;
+			background_rainbow_mode *= -1;
+		}
+		
+		switch(theme)
+		{
+			case 1:
+			default:
+				return al_map_rgb(0, 0, 0);
+			case 2:
+				return al_map_rgb(255, 255, 255);
+			case 3:
+				return al_map_rgb(255, 0, 0);
+			case 4:
+				return al_map_rgb(0, 255, 0);
+			case 5:
+				return al_map_rgb(0, 0, 255);
+			case 6:
+				return al_map_rgb(200, 0, 200);
+			case 7:
+				return al_map_rgb(0, 0, 0);
+			case 8:
+				return al_map_rgb(background_rainbow[0], background_rainbow[1], background_rainbow[2]);
+			case 9:
+				return al_map_rgb(background_rainbow[0], background_rainbow[1], background_rainbow[2]);
+			case 0:
+				return al_map_rgb(color_generator(rng), color_generator(rng), color_generator(rng));
+		}
+	}
+	
+	ALLEGRO_COLOR get_cell_color(int theme)
+	{
+		cell_rainbow[cell_rainbow_current] += cell_rainbow_mode;
+		if(cell_rainbow[cell_rainbow_current] % 250 == 0)
+		{
+			cell_rainbow_current++;
+			cell_rainbow_current %= 3;
+			cell_rainbow_mode *= -1;
+		}
+		
+		switch(theme)
+		{
+			case 1:
+			default:
+				return al_map_rgb(255, 255, 255);
+			case 2:
+				return al_map_rgb(0, 0, 0);
+			case 3:
+				return al_map_rgb(0, 0, 255);
+			case 4:
+				return al_map_rgb(255, 0, 0);
+			case 5:
+				return al_map_rgb(255, 255, 0);
+			case 6:
+				return al_map_rgb(0, 255, 0);
+			case 7:
+				return al_map_rgb(cell_rainbow[0], cell_rainbow[1], cell_rainbow[2]);
+			case 8:
+				return al_map_rgb(255, 255, 255);
+			case 9:
+				return al_map_rgb(cell_rainbow[0], cell_rainbow[1], cell_rainbow[2]);
+			case 0:
+				return al_map_rgb(color_generator(rng), color_generator(rng), color_generator(rng));
+		}
+	}
+	
+	void clear_board()
+	{
+		for(int i = 1; i <= WIDTH; i++)
+			for(int j = 1; j <= HEIGHT; j++)
+				cells[i][j] = false;
+	}
+	
 public:
 	game_of_life(){}
 	
 	// Initiates the board. Fills the array "cells" at random. 
 	void init()
 	{
-		std::random_device dev;
-		std::mt19937 rng(dev());
-		std::uniform_int_distribution<std::mt19937::result_type> dist(1, CHANCE);
+		cell_rainbow[0] = 0;
+		cell_rainbow[1] = 0;
+		cell_rainbow[2] = 250;
+		cell_rainbow_current = 1;
+		cell_rainbow_mode = 10;
+		
+		background_rainbow[0] = 250;
+		background_rainbow[1] = 250;
+		background_rainbow[2] = 0;
+		background_rainbow_current = 1;
+		background_rainbow_mode = -10;
+		
+		rng = std::mt19937(dev());
+		cell_generator = std::uniform_int_distribution<std::mt19937::result_type>(1, CHANCE);
+		color_generator = std::uniform_int_distribution<std::mt19937::result_type>(0, 255);
 		
 		for(int i = 1; i <= WIDTH; i++)
 			for(int j = 1; j <= HEIGHT; j++)
-				cells[i][j] = (dist(rng) == 1);
+				cells[i][j] = (cell_generator(rng) == 1);
 	}
 	
 	// Draws the board.
 	void draw()
 	{
-		al_clear_to_color(BACKGROUND_COLOR);
+		ALLEGRO_COLOR background_color;
+		ALLEGRO_COLOR cell_color;
+		{
+			std::unique_lock<std::mutex> lock(mut);
+			background_color = get_background_color(theme);
+			cell_color = get_cell_color(theme);
+		}
+		
+		al_clear_to_color(background_color);
 		for(int i = 1; i <= WIDTH; i++)
 			for(int j = 1; j <= HEIGHT; j++)
 				if(cells[i][j])
-					al_draw_filled_rectangle((i - 1) * CELL_SIZE, (j - 1) * CELL_SIZE, i * CELL_SIZE, j * CELL_SIZE, CELL_COLOR);
+					al_draw_filled_rectangle((i - 1) * CELL_SIZE, (j - 1) * CELL_SIZE, i * CELL_SIZE, j * CELL_SIZE, cell_color);
+		
 		al_flip_display();
 	}
 	
 	// Calculates one step of Game of Life.
-	void step()
+	// Returns false if game has ended.
+	// Returns true if game has not ended.
+	bool step()
 	{
+		{
+			std::unique_lock<std::mutex> lock(mut);
+			if(end_of_game)
+				return false;
+			if(pause_state)
+				return true;
+			if(restart_state)
+			{
+				init();
+				restart_state = false;
+			}
+			if(clear_state)
+			{
+				clear_board();
+				clear_state = false;
+			}
+		}
+		
 		for(int i = 1; i <= WIDTH; i++)
 		{
 			for(int j = 1; j <= HEIGHT; j++)
@@ -85,75 +221,47 @@ public:
 		for(int i = 1; i <= WIDTH; i++)
 			for(int j = 1; j <= HEIGHT; j++)
 				cells[i][j] = new_cells[i][j];
+				
+		return true;
 	}
-};
-
-// Class handling concurrent usage of game states.
-class concurrent_manager{
-private:
-	std::mutex mut;
-	bool end_of_game = false;
-	bool pause = false;
-	bool restart = false;
-	std::condition_variable cv_pause;
 	
-public:
-	void end_game()
+	void end()
 	{
 		std::unique_lock<std::mutex> lock(mut);
 		end_of_game = true;
-		cv_pause.notify_all();
 	}
 	
-	void change_pause()
+	void pause()
 	{
 		std::unique_lock<std::mutex> lock(mut);
-		pause = !pause;
-		cv_pause.notify_all();
+		pause_state = !pause_state;
 	}
 	
-	bool get_pause_state()
+	void restart()
 	{
 		std::unique_lock<std::mutex> lock(mut);
-		return pause;
+		restart_state = true;
 	}
 	
-	bool get_restart_state()
+	void clear()
 	{
 		std::unique_lock<std::mutex> lock(mut);
-		return restart;
+		clear_state = true;
 	}
 	
-	bool get_end_of_game_state()
+	void set_theme(int x)
 	{
 		std::unique_lock<std::mutex> lock(mut);
-		return end_of_game;
-	}
-	
-	void wait_on_pause()
-	{
-		std::unique_lock<std::mutex> lock(mut);
-		cv_pause.wait(lock, [this]{return (!pause || end_of_game);});
-	}
-	
-	void restart_do()
-	{
-		std::unique_lock<std::mutex> lock(mut);
-		restart = true;
-	}
-	
-	void restart_done()
-	{
-		std::unique_lock<std::mutex> lock(mut);
-		restart = false;
+		theme = x;
 	}
 };
 
+
+
 // Runs the game.
-// Stops when concurrent.pause == true.
-// Restarts when concurrent.restart == true.
-// Ends when concurrent.end_of_game == true.
-void game_loop(concurrent_manager & concurrent, game_of_life & game)
+// Makes game steps in loop.
+// Ends when game is ended.
+void game_loop(game_of_life & game)
 {
 	ALLEGRO_DISPLAY* disp = al_create_display(WIDTH * CELL_SIZE, HEIGHT * CELL_SIZE);
 	al_init_primitives_addon();
@@ -162,34 +270,34 @@ void game_loop(concurrent_manager & concurrent, game_of_life & game)
 	
 	while(run)
 	{
-		if(concurrent.get_pause_state())
-			concurrent.wait_on_pause();
-			
-		if(concurrent.get_end_of_game_state())
-			run = false;
-		
-		
-		if(run)
-		{
-			if(concurrent.get_restart_state())
-			{
-				game.init();
-				concurrent.restart_done();
-			}
-			game.draw();
-			game.step();
-			usleep(time);
-		}
+		game.draw();
+		run = game.step();
+		usleep(time);
 	}
 	
 	al_destroy_display(disp);
 }
 
+// Checks if given key code belongs to a number.
+bool is_a_number(int code)
+{
+	return code >= ALLEGRO_KEY_0 && code <= ALLEGRO_KEY_9;
+}
+
+
+// Given number key code returns the number.
+int get_number(int code)
+{
+	return code - ALLEGRO_KEY_0;
+}
+
 // Handles keyboard.
-// Changes concurrent.pause on when space is pressed.
-// Sets concurrent.restart to true when 'R' is pressed.
-// Sets concurrent.end_of_game to false when any other key is pressed.
-void keyboard_manager(concurrent_manager & concurrent)
+// Pauses the game when space is pressed.
+// Chaneges game theme when '1', '2', ... or '0' is pressed.
+// Restarts the game when 'R' is pressed.
+// Clears the borad (kills all cells) when 'C' is pressed.
+// Ends the game when any other key is pressed.
+void keyboard_manager(game_of_life & game)
 {
 
 	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
@@ -200,43 +308,45 @@ void keyboard_manager(concurrent_manager & concurrent)
 	al_start_timer(timer);
 	ALLEGRO_KEYBOARD_STATE state;
 	bool run = true;
-	bool space = false;
-	
 	while(run)
 	{
 		al_wait_for_event(queue, &event);
 		switch(event.type)
 		{
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				game.end();
+				run = false;
+				break;
+				
 			case ALLEGRO_EVENT_KEY_DOWN:
-			
 				al_get_keyboard_state(&state);
-				space = al_key_down(&state, ALLEGRO_KEY_SPACE);
 				switch(event.keyboard.keycode)
 				{
 					case ALLEGRO_KEY_R:
-						concurrent.restart_do();
-						break;
-					case ALLEGRO_KEY_SPACE:
-						concurrent.change_pause();
-						break;
-					default:
-						concurrent.end_game();
-						run = false;
+						game.restart();
 						break;
 						
-					
+					case ALLEGRO_KEY_SPACE:
+						game.pause();
+						break;
+						
+					case ALLEGRO_KEY_C:
+						game.clear();
+						break;
+						
+					default:
+						if(is_a_number(event.keyboard.keycode))
+							game.set_theme(get_number(event.keyboard.keycode));
+						else
+						{
+							game.end();
+							run = false;
+						}
+						break;
 				}
-				break;
-			
-			case ALLEGRO_EVENT_DISPLAY_CLOSE:
-			
-				concurrent.end_game();
-				run = false;
-				
 				break;
 		}
 	}
-	
 	al_destroy_timer(timer);
 	al_destroy_event_queue(queue);
 }
@@ -252,11 +362,10 @@ void init(game_of_life & game)
 int main()
 {
 	game_of_life game;
-	concurrent_manager concurrent;
 	init(game);
 	
-	std::thread loop([&concurrent, &game]{game_loop(concurrent, game);});
-	std::thread keyboard([&concurrent]{keyboard_manager(concurrent);});
+	std::thread loop([&game]{game_loop(game);});
+	std::thread keyboard([&game]{keyboard_manager(game);});
 	
 	keyboard.join();
 	loop.join();
